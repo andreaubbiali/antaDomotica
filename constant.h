e11dcc43f027a650fd2c6a4c166981ea585bebb4
aubbiali <uba99a@gmail.com>
2022-11-24 17:56:52 +0100

    Update constant

--- constant.h
+++ constant.h
@@ -4,3 +4,9 @@
 #define DS_STOP "3"
 
 #define STOP_DS "4"
+
+#define CLOSE_TIME "22:00"
+#define OPEN_TIME "06:00"
+
+// set this param to >= 5 to not continue check the open/close time and the photoresistence 
+#define AUTOMATIC_READ_MINUTE 1 
