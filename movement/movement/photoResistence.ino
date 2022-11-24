7612bf2fa9a3d56ec6cf3a4abfb7734892d5ff99
aubbiali <uba99a@gmail.com>
2022-11-24 21:35:12 +0100

    implemented photoresistence read

--- movement/movement/photoResistence.ino
+++ movement/movement/photoResistence.ino
@@ -1,25 +1,31 @@
-const uint photoResistenceUpperLimit = 100;
-const uint photoResistenceLowerLimit = 50;
+// If the value readed from photoresistence is under this constant, the door should be closed
+const uint photoResistenceLimit = 250;
+const uint readPhotoResistenceMinutes = 2;
 
 /**
 * Read photoresistence
 */
-void readPhotoresistence() {
+String readPhotoresistence() {
+  uint startRead = millis();
+  uint closeCount = 0;
+  uint openCount = 0;
 
-  if (countPhotoResistenceUp == 4) {
-    // closeDoor();
-    countPhotoResistenceUp = 0;
-  } else if (countPhotoResistenceLow == 4) {
-    // openDoor();
-    countPhotoResistenceLow = 0;
-  }
-  
-  uint val = analogRead(A0);
-  if (val > photoResistenceUpperLimit) {
-    countPhotoResistenceUp += 1;
+  // read until you reach n minutes of read or switch from automatic to manual (button pressed) 
+  while(!isManual && (millis() < (startRead + (readPhotoResistenceMinutes*60000)))){
+    uint val = analogRead(A0);
+    Serial.println("PHOTO VAL: " + String(val));
+    if (val < photoResistenceLimit) {
+      closeCount++;
+    } else {
+      openCount++;
+    }
+    delay(200);
+    mqttClient.loop();
   }
 
-  if (val < photoResistenceLowerLimit) {
-    countPhotoResistenceLow += 1;
+  if (closeCount>openCount){
+    return CLOSE;
+  } else {
+    return OPEN;
   }
 }
